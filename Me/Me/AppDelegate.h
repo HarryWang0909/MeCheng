4af997344142c28c4df206325189cc8ae410450b
manager <12345.com>
2016-07-11 18:47:09 +0800

    初始化项目

---
+++ Me/Me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Me
+//
+//  Created by 王慧 on 16/7/11.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
